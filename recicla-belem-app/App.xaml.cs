fd0356efa84a402c5ee057c6d154e6c0ae59541d
BK Antunes <bkarinesantos@hotmail.com>
2025-08-16 16:31:51 -0300

    Adicionar arquivos ao projeto.

---
+++ recicla-belem-app/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace recicla_belem_app
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
